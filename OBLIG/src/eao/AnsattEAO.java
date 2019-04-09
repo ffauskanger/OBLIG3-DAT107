28367f747344ecbc45011836ca78b6753a54e738
ffauskanger <33883489+ffauskanger@users.noreply.github.com>
2019-04-09 14:58:29 +0200

    finnAnsatt

--- OBLIG/src/eao/AnsattEAO.java
+++ OBLIG/src/eao/AnsattEAO.java
@@ -1,5 +1,24 @@
 package eao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import entities.Ansatt;
+
 public class AnsattEAO {
+	
+	public static Ansatt finnAnsattMedId(int id) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AnsattPersistenceUnit");
+        EntityManager em = emf.createEntityManager();
+        Ansatt a = null;
+
+        try {
+            a = em.find(Ansatt.class, id);
+        } finally {
+            em.close();
+        }
+        return a;
+    }
 
 }
